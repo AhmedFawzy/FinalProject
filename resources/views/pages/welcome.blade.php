518aab0e489533331e9a1a1d6cfde079f2eeb145
Sakina Shivji <shivjisakina@gmail.com>
2017-08-12 22:50:23 -0400

    trying to use zillow api php wrapper

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -9,16 +9,6 @@
 
     <title>Welcome</title>
 
-
-        <!-- Bootstrap core CSS     -->
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet"/>
-
-        <!--  Material Dashboard CSS    -->
-        <link href="../assets/css/material-dashboard.css" rel="stylesheet"/>
-
-        <!--  CSS for Demo Purpose, don't include it in your project     -->
-        <link href="../assets/css/demo.css" rel="stylesheet"/>
-
         <!--     Fonts and icons     -->
         <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
         <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet'
@@ -45,24 +35,24 @@
 $client = new \GuzzleHttp\Client();
 
 $res = $client->request('GET', 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1fx492w1h57_3csw9&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA ');
-//echo $res->getStatusCode();
-// 200
-//echo $res->getHeaderLine('content-type');
-// 'application/json; charset=utf8'
 echo $res->getBody();
-// '{"id": 1420053, "name": "guzzle", ...}'
 
 // Send an asynchronous request.
 $request = new \GuzzleHttp\Psr7\Request('GET', 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1fx492w1h57_3csw9&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA ');
 
-/*$xml = XmlParser::load('path/to/above.xml');
-$address = $xml->parse([
-    'street' => ['uses' => 'address.street'],
-    'zip' => ['uses' => 'address.zipcode'],
-    'city' => ['uses' => 'address.city'],
-    'state' => ['uses' => 'address.state'],
-]);
+$request_url = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1fx492w1h57_3csw9&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA";
 
-var_dump($xml);*/
+$client = new \GuzzleHttp\Client();
+$response = $client->request('GET', $request_url, [
+    'headers' => ['Accept' => 'application/xml'],
+    'timeout' => 120
+])->getBody()->getContents();
+
+use Zillow\ZillowClient;
+
+$client = new ZillowClient('ZWSID');
+
+$response = $client->GetSearchResults(['address' => '5400 Tujunga Ave', 'citystatezip' => 'North Hollywood, CA 91601']);
+
 
 
