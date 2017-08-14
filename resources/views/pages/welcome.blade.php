fd62017eca1641012bc6c4ce0798c3f2ee5e3f96
Sakina Shivji <shivjisakina@gmail.com>
2017-08-14 13:57:48 -0400

    parsed through xml and got address and city

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -19,9 +19,9 @@
 </head>
 
 <body>
-<form action="/" method="POST">
+<form action="/" method="POST" class="text-center">
     Search by address:<br>
-    <input type="text" name="search" value="Townhomes at Lucaya Lake Club by Ryan Homes"><br>
+    <input type="text" name="search" value="2114 Bigelow Ave"><br>
     <button type="submit">Submit</button>
 </form>
 
@@ -29,25 +29,52 @@
 
 </html>
 
-@include('includes.footer')
 
 <?php
 
 
 require('simple_html_dom.php');
 
-/*$client = new \GuzzleHttp\Client();
+$client = new \GuzzleHttp\Client();
 
 $res = $client->request('GET', 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1fx492w1h57_3csw9&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA ');
-echo $res->getBody();
+$res2 = $client->request('GET', 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1fx492w1h57_3csw9&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA');
+$xmlParse = ($res->getBody());
 
-// Send an asynchronous request.
-$request = new \GuzzleHttp\Psr7\Request('GET', 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1fx492w1h57_3csw9&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA ');
+libxml_use_internal_errors(true);
 
-$request_url = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1fx492w1h57_3csw9&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA";
+$xml = simplexml_load_string($xmlParse);
+if ($xml === false) {
+    echo "Failed loading XML: ";
+    foreach(libxml_get_errors() as $error) {
+        echo "<br>", $error->message;
+    }
+} else {
+    $address = ($xml->request->address);
+    echo "<h1 class='text-center'>" . $address . "</h1>" . "<br>";
+
+    $citystatezip = ($xml->request->citystatezip);
+    echo "<h1 class='text-center'>" . $citystatezip . "</h1>" . "<br>";
+
+    var_dump($xml->response->results->result);
+}
 
 
-$client = new \GuzzleHttp\Client();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$client = new \GuzzleHttp\Client();
 $response = $client->request('GET', $request_url, [
     'headers' => ['Accept' => 'application/xml'],
     'timeout' => 120
@@ -65,7 +92,12 @@ $response = $client->GetSearchResults(['address' => '5400 Tujunga Ave', 'citysta
 $html = file_get_contents($url);
 echo $html;*/
 
-$html = file_get_html('https://www.trulia.com/builder-community/Townhomes-at-Lucaya-Lake-Club-3268866340/');
+/*$html = file_get_html('https://www.trulia.com/builder-community/Townhomes-at-Lucaya-Lake-Club-3268866340/');
 
-    var_dump($html->root->nodes);
+var_dump($html);*/
+
+/*foreach ( $array->nodes as $home ) {
+//	var_dump( $gif->images->original);
+    echo $home;
+}*/
 
